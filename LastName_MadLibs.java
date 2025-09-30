edc645587f7debcd9f9ea32e728d9c8d78d84403
SantaRitaRoad <157430224+SantaRitaRoad@users.noreply.github.com>
2025-09-29 20:47:13 -0700

    Update LastName_MadLibs.java

--- LastName_MadLibs.java
+++ LastName_MadLibs.java
@@ -1,1 +1,21 @@
-
+public class LastName_MadLibs() {
+  public static void main(String[] args) {
+    /* madlib framing use for later when implementing:
+    One day a [animal] walked into a [place].
+    He then asked a [animal] what was the time.
+    The [same animal] responded 11:51.
+    This meant [person’s name] had 9 minutes left before he [verb].
+    
+    Panicking, the [animal] grabbed a [object] and ran toward the [vehicle] parked outside.
+    Along the way, a [adjective] [creature] tried to stop him by yelling, “[funny phrase]!”
+    But there was no time to waste.
+    
+    He sprinted past the [place, plural], through a crowd of [plural noun], and into the [adjective] building.
+    Inside, he found a [object] glowing with a [color] light.
+    Picking it up, he realized it was the key to saving [silly noun].
+    
+    With only seconds left, the [animal] shouted to [person’s name], “We must [verb] before it’s too late!”
+    The clock struck [time], and suddenly everything turned [color].
+    */
+  }
+}
